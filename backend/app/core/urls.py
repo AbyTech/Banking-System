a45ab450410d650b6d0fb086d863ec284eec241f
AbyTech <abrahamisaacjames22@gmail.com>
2025-11-16 02:02:31 +0100

    New update

--- backend/app/core/urls.py
+++ backend/app/core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -13,6 +14,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/docs/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/auth/', include('app.users.urls')),
     path('api/bank/', include('app.bank.urls')),
