e35535708a1dab17ddcd4832827a0a09b2ce28c2
AbyTech <abrahamisaacjames22@gmail.com>
2025-11-16 02:58:42 +0100

    New

--- backend/app/core/urls.py
+++ backend/app/core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.views.generic.base import RedirectView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -11,6 +12,7 @@ schema_view = get_schema_view(
         description="API for Primewave Bank",
     ),
     public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
